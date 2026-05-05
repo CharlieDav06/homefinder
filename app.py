e3dbcda70d2cad0be45a6c635751210d9a030274
CharlieD06 <100742585@unimail.derby.ac.uk>
2026-05-05 21:11:27 +0000

    added requirements and procfile

--- app.py
+++ app.py
@@ -4,10 +4,12 @@ from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'homefinder123'
-app.config['MYSQL_DB'] = 'home_finder_db'
+import os
+
+app.config['MYSQL_HOST'] = os.environ.get('MYSQL_HOST')
+app.config['MYSQL_USER'] = os.environ.get('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = os.environ.get('MYSQL_PASSWORD')
+app.config['MYSQL_DB'] = os.environ.get('MYSQL_DB')
 
 mysql = MySQL(app)
 
